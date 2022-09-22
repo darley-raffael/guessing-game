6a3603624d2342ee1b73f70a775edd0501d37b31
DarleyRaffael <darleyraffael@gmail.com>
2022-09-22 07:15:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Jogo de Adivinhar números
+
+![preview game](./.github/preview-game.png)
+---
+>💡 Este projeto foi desenvolvido no curso Explorer da Rocketseat
+
+## 🛠 Ferramentas Utilizadas
+- HTML
+- CSS
+- JavaScript
+- Git & Github
+
+## 📚 Alguns conteúdos Abordados
+- Manipulação da DOM
+- Eventos com JavaScript
+- Funções
+- Refatoração do código.
+
+## 🔗 Links 
+
+🎮 [**Gessing Game**](https://darley-raffael.github.io/gessing-game/)
---
+++ main.js
@@ -0,0 +1,44 @@
+// VARIÁVEIS
+const bntTry = document.querySelector('#bntTry')
+const bntReset = document.querySelector('#bntReset')
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// EVENTOS
+bntTry.addEventListener('click', handleTryClick)
+bntReset.addEventListener('click', handleResetClick) 
+// document.addEventListener('keydown', function(e){
+//     if(e.key == 'Enter') {
+//         handleResetClick()
+//     }
+// })
+
+// FUNÇÃO CALLBACK
+function handleTryClick(event){
+    event.preventDefault() // Não executar o evento padrão do botão
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if (Number(inputNumber.value) == randomNumber) {
+        screen1.classList.add('hide')
+        screen2.classList.remove('hide')
+
+        document.querySelector('.screen2 h2')
+        .innerText = `Você acertou em ${xAttempts} tentativas` 
+    } 
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetClick () {
+    toggleScreen()
+    xAttempts = 1
+}
+
+function toggleScreen(){
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
+}
+
